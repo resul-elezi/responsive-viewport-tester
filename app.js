4891cba9d1fd67621b5ab53fa18e183db43da295
Resul Elezi <resul_elezi@protonmail.com>
2026-07-27 14:56:04 +0200

    defined loadUrl function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+    // DOM Elements Reference
     const urlInput = document.getElementById('url-input');
     const loadBtn = document.getElementById('load-btn');
     const iframe = document.getElementById('viewport-iframe');
@@ -11,4 +12,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const screenshotBtn = document.getElementById('screenshot-btn');
     const notesToggleBtn = document.getElementById('notes-toggle-btn');
     const notesSidebar = document.getElementById('notes-sidebar');
+
+    // 1. Loading URL in the Iframe
+    function loadUrl() {
+        let url = urlInput.value.trim();
+        if (!url.startsWith('http://') && !url.startsWith('https://')) {
+            url = 'https://' + url;
+            urlInput.value = url;
+        }
+        iframe.src = url;
+        // Enable screenshot button as soon as a URL is loaded
+        screenshotBtn.removeAttribute('disable');
+    }
+
+    loadBtn.addEventListener('click', loadUrl);
+    urlInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            loadUrl();
+        }
+    });
+
+    
 })
